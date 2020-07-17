76795c452e46b0cee04fa876eee43c182174496d
Jared Bearss <Jared.D.Bearss@EMCIns.com>
2020-07-17 08:40:12 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def route(func):
+    def inner(url):
+        func()
+    return inner
+
 @route("/")
 @route("/jobs")
 def jobs():
-    render_template(index.html)
+    render_template("index.html")
